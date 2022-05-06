c74553e4f28c2f671a34b56744c7190046ab05b3
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-05-06 14:54:45 +0200

    feat(webcam): totally black borders

--- src/app/Webcam.tsx
+++ src/app/Webcam.tsx
@@ -1,5 +1,5 @@
 export default function Webcam() {
   return (
-    <div className="absolute bottom-8 left-8 border-8 shadow-[inset_0_0_32px_8px_rgba(0,0,0,0.9)] rounded-xl w-64 h-64 border-[rgba(0,0,0,0.9)]"></div>
+    <div className="absolute bottom-8 left-8 border-8 shadow-[inset_0_0_32px_8px_rgba(0,0,0)] rounded-xl w-64 h-64 border-black"></div>
   );
 }
